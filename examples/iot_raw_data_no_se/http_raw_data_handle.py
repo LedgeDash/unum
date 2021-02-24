a21c37d3ff3ce5de0576fe0288c69297aedba6f0
David H. Liu <hl7@cs.princeton.edu>
2021-02-24 00:32:41 -0600

    nits

--- examples/iot_raw_data_no_se/http_raw_data_handle.py
+++ examples/iot_raw_data_no_se/http_raw_data_handle.py
@@ -1,9 +1,10 @@
 from my_faas_functions import aggregator_raw_data_no_se, hvac_controller_raw_data_no_se
-import requests
+import requests, json
 
 def handle(event):
-	'''This function is expected to be invoked via HTTP requests, not S3 events
+	'''This function is expected to be invoked via HTTP requests, not S3
+	events
 	'''
-	action = hvac_controller_raw_data_no_se(aggregator_raw_data_no_se(event))
+	action = hvac_controller_raw_data_no_se(aggregator_raw_data_no_se(json.dumps(event)))
 
 	requests.get(actuator_url, data = action)
