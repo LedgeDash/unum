66ef5975458cd17aa29abc852b256df63515b20d
David H. Liu <hl7@cs.princeton.edu>
2021-08-25 17:11:08 -0500

    SF frontend fixes and more tests

--- frontend/step-functions/sf.py
+++ frontend/step-functions/sf.py
@@ -46,148 +46,6 @@ def get_state_unum_function_name(state):
     else:
         return state["Resource"]
 
-def _get_map_continuation(state):
-
-    if state["Type"] != "Map":
-        raise
-
-    iterator = state["Iterator"]
-    entry_state = iterator["States"][iterator["StartAt"]]
-
-    if entry_state["Type"] == "Task":
-        return {
-                "Next": {
-                    "Name": get_state_unum_function_name(entry_state)
-                },
-                "NextInput": "Map"
-            }
-    elif entry_state["Type"] == "Parallel":
-        parallel_cnt = _get_parallel_continuation(entry_state)
-        if parallel_cnt["NextInput"] == "Map":
-            pass
-        #TODO
-    elif entry_state["Type"] == "Map":
-        # This case doesn't make sense
-        return None
-    elif entry_state["Type"] == "Choice":
-        pass
-
-
-def _get_parallel_continuation(state):
-    if state["Type"] != "Parallel":
-        raise
-
-    cnts = []
-    # each branch is a state machine
-    for b in state["Branches"]:
-        entry_state = b["States"][b["StartAt"]]
-        branch_continuation = _get_continuation(entry_state)
-        if branch_continuation["NextInput"] == "Scalar":
-            # the state is of type "Task"
-            cnts.append(branch_continuation)
-        elif branch_continuation["NextInput"] == "Map":
-            cnts.append(branch_continuation)
-        else:
-            pass
-    ret = {"Next":[], "NextInput": cnts[0]["NextInput"]}
-    for c in cnts:
-        ret["Next"].append(c["Next"])
-    print(ret)
-    return ret
-
-def _get_continuation(state):
-    ''' Given a state, return the continuation into it. State here can be a
-    state machine in the case of Map Iterator and Parallel branches
-    '''
-    if state["Type"] == "Task":
-        return {
-            "Next": {
-                "Name": get_state_unum_function_name(state)
-            },
-            "NextInput": "Scalar"
-        }
-
-    elif state["Type"] == "Map":
-        return _get_map_continuation(state)
-    elif state["Type"] == "Parallel":
-        return _get_parallel_continuation(state)
-    elif state["Type"] == "Pass":
-        pass
-    elif state["Type"] == "Wait":
-        pass
-    elif state["Type"] == "Choice":
-        pass
-    elif state["Type"] == "Succeed":
-        return None
-    elif state["Type"] == "Fail":
-        return None
-
-
-
-def get_continuation(state, state_machine):
-    ''' Given a state, compute its continuation based on its "Next" and "End"
-    field. Return the continuation as a dict.
-    '''
-    if "End" in state and state["End"] == True:
-        return None
-
-    try:
-        next_state = state_machine["States"][state["Next"]]
-        return _get_continuation(next_state)
-    except KeyError as e:
-        raise e
-
-
-def translate_task(state_name, state, state_machine):
-    ''' Given a Step Function Task state, return its unum config
-    '''
-
-    # Get the unum function name from the Resource field
-    # This field will be the directory where the new config file lives
-    unum_function_name = get_state_unum_function_name(state)
-    print(f'{state_name}: {state}\nFunction name: {unum_function_name}')
-
-    config = {
-        "Name": unum_function_name,
-        "State ID": state_name
-    }
-
-    continuation = get_continuation(state, state_machine)
-    if continuation == None:
-        return config
-    else:
-        return {**config, **continuation}
-
-
-
-def translate_state(state_name, state_machine):
-    ''' Given a state in a Step Function state machine definition, generate
-    its unum-config in the form of a python dict.
-
-    Note that the state can be container states such as Map and Parallel, in
-    which case this function returns a list of dict.
-
-    @param state_name
-    @param state_machine
-
-    @return unum_config dict
-    '''
-    this_state = state_machine["States"][state_name]
-    # print(f'{state_name}: {this_state}')
-
-    if this_state["Type"] == "Task":
-        return translate_task(state_name, this_state, state_machine)
-    elif this_state["Type"] == "Map":
-        return translate_map(state_name, this_state, state_machine)
-    elif this_state["Type"] == "Parallel":
-        return translate_parallel(state_name, this_state, state_machine)
-    elif this_state["Type"] == "Choice":
-        return translate_choice(state_name, this_state, state_machine)
-    elif this_state["Type"] == "Pass" or this_state["Type"] == "Wait" or this_state["Type"] == "Succeed" or this_state["Type"] == "Fail":
-        return
-    else:
-        raise ValueError(f'Unknown State type')
-
 
 def _translate_state_machine(state_name, state_machine):
     ''' Given a state, compute the IR, Entry function's config and exit
@@ -236,7 +94,7 @@ def _translate_state_machine(state_name, state_machine):
     elif state["Type"] == "Map":
 
         iterator = translate_state_machine(state["Iterator"])
-
+        global unum_map_counter
         unum_map = {
             "Name": f'UnumMap{unum_map_counter}',
             "Next": iterator["Entry unum function"]["Name"],
@@ -283,17 +141,27 @@ def _translate_state_machine(state_name, state_machine):
 
     elif state["Type"] == "Parallel":
         branches = [translate_state_machine(b) for b in state["Branches"]]
+        global unum_parallel_counter
         unum_parallel = {
             "Name": f'UnumParallel{unum_parallel_counter}',
             "Next": [b["Entry unum function"]["Name"] for b in branches],
             "NextInput":"Scalar"
         }
+        ir.append(unum_parallel)
+
         unum_parallel_sink = {
             "Name": f'UnumParallelSink{unum_parallel_counter}'
         }
-        ir.append(unum_parallel)
+        unum_parallel_counter = unum_parallel_counter +1
+        parallel_fan_in_vals = [f'{branches[i]["Exit unum function"]["Name"]}-unumIndex-{i}' for i in range(len(branches))]
         for b in branches:
             ir = ir + b["unum IR"]
+            b["Exit unum function"]["NextInput"] = {
+                "Fan-in": {
+                    "Values": parallel_fan_in_vals
+                }
+            }
+            b["Exit unum function"]["Next"] = unum_parallel_sink["Name"]
 
         ir.append(unum_parallel_sink)
 
@@ -360,7 +228,10 @@ def main():
         template = load_yaml(f.read())
 
     for c in ir["unum IR"]:
-        c["Checkpoint"] = template["Globals"]["Checkpoint"]
+        if "NextInput" in c and "Fan-in" in c["NextInput"]:
+            c["Checkpoint"] = True
+        else:
+            c["Checkpoint"] = template["Globals"]["Checkpoint"]
 
     print("**************** IR ***************")
     print(f'{ir}')
